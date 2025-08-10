119abe99a282f5dd9d504db8618e729b953e8eb3
AjoobKansakar <ajoob.sagar@gmail.com>
2025-08-10 21:20:43 +0545

    updated login page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,41 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import LoginGuard from './Shared/config/guards/login_guard'
-import Login from './Features/Login/login'
-import Register from './Features/Register/register'
-import AuthGuard from './Shared/config/guards/auth_guard'
-import Home from './Features/Homepage/homepage'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginGuard from "./Shared/config/guards/login_guard";
+import Login from "./Features/Login/login";
+import Register from "./Features/Register/register";
+import AuthGuard from "./Shared/config/guards/auth_guard";
+import Home from "./Features/Homepage/homepage";
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" replace />} />
-        <Route path='/login' element={
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route
+        path="/login"
+        element={
           <LoginGuard>
             <Login />
           </LoginGuard>
-        } />
-        <Route path='/register' element={
+        }
+      />
+      <Route
+        path="/register"
+        element={
           <LoginGuard>
             <Register />
           </LoginGuard>
-        } />
-        <Route path='/home' element={
+        }
+      />
+      <Route
+        path="/home"
+        element={
           <AuthGuard>
             <Home />
           </AuthGuard>
-        } />
-      </Routes>
-  )
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Features/Login/login.tsx
+++ frontend/src/Features/Login/login.tsx
@@ -1,64 +1,82 @@
-import { useState, type ChangeEvent, type FormEvent } from "react"
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import "./login.css";
 import type { AxiosError, AxiosResponse } from "axios";
 import { login } from "../../Shared/config/api";
-import { useNavigate } from "react-router-dom"
-
-
+import { useNavigate } from "react-router-dom";
+import UserIcon from "../../assets/User-Icon.svg";
 
 export default function Login() {
-    const navigate = useNavigate();
-    
-    const [ formData, setFormData ] = useState ({ username: '', password: ''});
+  const navigate = useNavigate();
 
-    const [ loading, setloading] = useState(false);
+  const [formData, setFormData] = useState({ username: "", password: "" });
 
-    const handlechange = (e: ChangeEvent <HTMLInputElement> ) => {
-        const {name, value} = e.target;
+  const [loading, setloading] = useState(false);
 
-        setFormData ({...formData, [name]: value}) 
+  const handlechange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (loading) {
+      return;
     }
 
-    const handleSubmit = (e: FormEvent <HTMLFormElement>) => {
-        e.preventDefault();
-
-        if(loading) {
-            return;
-        }
-
-        setloading(true);
-        login(formData).then( (res: AxiosResponse) => {
-            console.log("Login response:", res.data);
-            localStorage.setItem('token', res.data.token);
-            localStorage.setItem('currentUser', JSON.stringify(res.data.user));
-            navigate('/home');
-        }) .catch(
-            (error: AxiosError) => {
-                console.log(error);
-    } 
-).finally (
-    () => {
+    setloading(true);
+    login(formData)
+      .then((res: AxiosResponse) => {
+        console.log("Login response:", res.data);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("currentUser", JSON.stringify(res.data.user));
+        navigate("/home");
+      })
+      .catch((error: AxiosError) => {
+        console.log(error);
+      })
+      .finally(() => {
         setloading(false);
-    }
-)
+      });
+  };
+  return (
+    <div className="container">
+      {/* Icon */}
+      <div className="profile-icon">
+        <img src={UserIcon} alt="User Icon" />
+      </div>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          className="input-box"
+          onChange={handlechange}
+          value={formData.username}
+          placeholder="Enter Username"
+        />
+        <input
+          type="password"
+          name="password"
+          className="input-box"
+          onChange={handlechange}
+          value={formData.password}
+          placeholder="Enter Password"
+        />
+        <button type="submit" className="login-button">
+          LOGIN
+        </button>{" "}
+        <span onClick={() => navigate("/home")}></span>
+        <p className="swicher">
+          Don't have an account? {""}
+          <span
+            onClick={() => navigate("/register")}
+            style={{ color: "blue", cursor: "pointer" }}
+          >
+            Register here
+          </span>
+        </p>
+      </form>
+    </div>
+  );
 }
-    return (
-    
-        <div className="container">
-            <form className="login-form" onSubmit={handleSubmit}>
-                <h1> Login </h1>
-                <input type="text" name="username" onChange={handlechange}  value={ formData.username } placeholder="Enter Username"/>
-                <input type="password" name="password" onChange={handlechange} value={ formData.password } placeholder="Enter Password"/>
-                <button type="submit" className="login-button">login</button> <span onClick={() => navigate('/home')}></span>
-
-                <p className="swicher">
-                    Don't have an account? {""}
-                    < span onClick={() => navigate('/register')} style={{ color: 'white', cursor: 'pointer',}}>
-                        Register here
-                    </span>
-                </p>
-            </form>
-        </div>
-    );
-
-};
